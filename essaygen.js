baef12113a2380d26d3d872e91bd89cd113824b4
z------------- <zacharyguard@gmail.com>
2014-05-10 17:27:29 +0800

    Added adjectives

--- essaygen.js
+++ essaygen.js
@@ -6,6 +6,7 @@ var essaygen = {
         var techniques = ["alliteration", "simile", "metaphor", "personification"];
         var writerName = "the writer";
         var outputType = "default";
+        var adjectives = ["lively", "realistic", "cheerful", "dramatic"];
         
         var lineBreaker = "\n";
         
@@ -25,6 +26,9 @@ var essaygen = {
             if ("output" in options) {
                 outputType = options.output;
             }
+            if ("adjectives" in options) {
+                adjectives = options.adjectives;
+            }
         }
         
         if (outputType == "html") {
@@ -38,16 +42,18 @@ var essaygen = {
         var points = [
             [
                 "The writer skilfully uses language and techniques like " + randomInArray(techniques) + " and " + randomInArray(techniques) + " to effectively convey his message.",
-                "In this " + litType + ", " + writerName + " tries to emphasize " + protagonist + "'s personality."
+                "In this " + litType + ", " + writerName + " tries to emphasize " + protagonist + "'s personality.",
+                "The writer uses a combination of " + randomInArray(techniques) + " and " + randomInArray(techniques) + " to bring out " + protagonist + "'s " + randomInArray(adjectives) + " thoughts and feelings to the reader."
             ],[
-                "An example of this can be found in this quote:"
+                "An example of this can be found in this quote:",
+                "This makes the " + litType + " seem more " + randomInArray(adjectives) + " and " + randomInArray(adjectives) + ", as shown in the following quote:"
             ]
         ];
         var explanations = [
             [
-                "This suggests that " + writerName + " feels very strongly about the situation, as shown in the " + randomInArray(techniques) + " in the above quote. It causes the reader to associate the two things, which adds the atmosphere of the " + litType +"."
+                "This suggests that " + writerName + " feels very strongly about the situation, as shown in the " + randomInArray(techniques) + " in the above quote. It causes the reader to associate the two things, which adds to the " + randomInArray(adjectives) + " atmosphere of the " + litType +"."
             ],[
-                "Also, the writer effectively uses " + randomInArray(techniques) + " in his " + litType + ", to let the reader experience " + protagonist + "'s feelings."
+                "Also, the writer effectively uses " + randomInArray(techniques) + " in his " + litType + ", to let the reader experience " + protagonist + "'s " + randomInArray(adjectives) + " feelings."
             ]
         ];
         var finalEssay = [];
