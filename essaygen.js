2519ae0e4b4c0491735ecac34a0d100652ad3d10
z------------- <zacharyguard@gmail.com>
2014-05-10 16:09:03 +0800

    More options, syntax change

--- essaygen.js
+++ essaygen.js
@@ -1,33 +1,65 @@
 var essaygen = {
-    generate: function(quotes,litType) {
+    generate: function(quotes,options) {
+        // set option defaults
+        var litType = "short story";
+        var protagonist = "the protagonist";
+        var techniques = ["alliteration", "simile", "metaphor", "personification"];
+        var writerName = "the writer";
+        var outputType = "default";
+        
+        var lineBreaker = "\n";
+        
+        if (options) {
+            if ("type" in options) {
+                litType = options.type;
+            }
+            if ("protagonist" in options) {
+                protagonist = options.protagonist;
+            }
+            if ("protagonist" in options) {
+                techniques = options.techniques;
+            }
+            if ("writer" in options) {
+                writerName = options.writer;
+            }
+            if ("output" in options) {
+                outputType = options.output;
+            }
+        }
+        
+        if (outputType == "html") {
+            lineBreaker = "<br>";
+        }
+        
+        var randomInArray = function(array){
+            return array[Math.round(Math.random()*(array.length-1))];
+        }
+
         var points = [
             [
-                "The writer skilfully uses language and techniques like alliteration and simile to effectively convey his message.",
-                "In this " + litType + ", the writer tries to emphasize the protagonist's personality."
+                "The writer skilfully uses language and techniques like " + randomInArray(techniques) + " and " + randomInArray(techniques) + " to effectively convey his message.",
+                "In this " + litType + ", " + writerName + " tries to emphasize " + protagonist + "'s personality."
             ],[
                 "An example of this can be found in this quote:"
             ]
         ];
         var explanations = [
             [
-                "This suggests that the writer feels very strongly about the situation, as shown in the alliteration in the above quote. It causes the reader to associate the two things, which adds the atmosphere of the " + litType +"."
+                "This suggests that " + writerName + " feels very strongly about the situation, as shown in the " + randomInArray(techniques) + " in the above quote. It causes the reader to associate the two things, which adds the atmosphere of the " + litType +"."
             ],[
-                "Also, the writer effectively uses metaphor in his " + litType + ", to let the reader experience the protagonist's feelings."
+                "Also, the writer effectively uses " + randomInArray(techniques) + " in his " + litType + ", to let the reader experience " + protagonist + "'s feelings."
             ]
         ];
-        var finalEssay = "";
-        var randomInArray = function(array){
-            return array[Math.round(Math.random()*(array.length-1))];
-        }
+        var finalEssay = [];
         
         for (i=0; i<quotes.length; i++) {
             var point = randomInArray(points[0]) + " " + randomInArray(points[1]);
             var quote = quotes[i];
             var explanation = randomInArray(explanations[0]) + " " + randomInArray(explanations[1]);
-            var paragraph = point + " " + "\"" + quote + "\"" + ". " + explanation + "\n";
-            finalEssay += paragraph;
+            var paragraph = point + " " + "\"" + quote + "\"" + ". " + explanation;
+            finalEssay.push(paragraph);
         }
         
-        return finalEssay;
+        return finalEssay.join(lineBreaker);
     }
 };
