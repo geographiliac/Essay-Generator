cd8c519dc1dc2cb213ef6aeb355c547eafb6adbb
Zachary Guard <zacharyguard@gmail.com>
2014-05-12 12:44:30 +0800

    Variable nouns + more structures
    Also made code slightly more readable.

--- essaygen.js
+++ essaygen.js
@@ -7,7 +7,8 @@ var essaygen = {
         var techniques = ["alliteration", "simile", "metaphor", "personification", "listing", "anthropomorphism", "assonance", "euphemism", "pathetic fallacy"];
         var writerName = "the writer";
         var outputType = "default";
-        var adjectives = ["lively", "realistic", "cheerful", "dramatic", "happy", "joyous", "carefree", "radiant", "lighthearted"];
+        var adjectives = ["lively", "realistic", "cheerful", "dramatic", "happy", "joyous", "carefree", "radiant", "lighthearted", "selfish", "selfless","vain", "greedy"];
+        var nouns = ["sorrow", "kindness", "happiness", "joy", "vanity", "lightheartedness", "greediness", "radiance", "personality", "kindheartedness", "charity", "selflessness", "selfishness"];
         
         if (options) {
             if ("type" in options) {
@@ -27,6 +28,9 @@ var essaygen = {
             }
             if ("adjectives" in options) {
                 adjectives = options.adjectives;
+            }
+            if ("nouns" in options) {
+                nouns = options.nouns;
             }
         }
         
@@ -52,8 +56,16 @@ var essaygen = {
         }
         
         var replaceVariables = function(string) {
-            while (string.indexOf("%t%") != -1 || string.indexOf("%l%") != -1 || string.indexOf("%w%") != -1 || string.indexOf("%p%") != -1 || string.indexOf("%a%") != -1 || string.indexOf("%q%") != -1 || string.indexOf("%qw%") != -1) {
-                string = string.replace("%t%",randomInArray(techniques)).replace("%l%",litType).replace("%w%",writerName).replace("%p%",protagonist).replace("%a%",randomInArray(adjectives)).replace("%q%",quotes[i]).replace("%qw%",randomWord(quotes[i]));
+            while (string.indexOf("%t%") != -1 || string.indexOf("%l%") != -1 ||
+                   string.indexOf("%w%") != -1 || string.indexOf("%p%") != -1 ||
+                   string.indexOf("%a%") != -1 || string.indexOf("%q%") != -1 ||
+                   string.indexOf("%qw%") != -1 || string.indexOf("%n%") != -1) {
+                    string = string.replace("%t%",randomInArray(techniques))
+                                   .replace("%l%",litType).replace("%w%",writerName)
+                                   .replace("%p%",protagonist)
+                                   .replace("%a%",randomInArray(adjectives))
+                                   .replace("%q%",quotes[i]).replace("%qw%",randomWord(quotes[i]))
+                                   .replace("%n%",randomInArray(nouns));
             }
             return string;
         }
@@ -62,19 +74,22 @@ var essaygen = {
             [
                 "The writer skilfully uses language and techniques like %t% and %t% to effectively convey his message.",
                 "In this %l%, %w% tries to emphasize %p%'s personality.",
-                "The writer uses a combination of %t% and %t% to bring out %p%'s %a% thoughts and feelings to the reader."
+                "The writer uses a combination of %t% and %t% to bring out %p%'s %a% thoughts and feelings to the reader.",
+                "One of the ways that %w% makes the %l% more exciting is by focusing on %p%'s %n%. This makes the reader feel more familiar with them and lets the reader feel their %n% and %n%."
             ],[
                 "An example of this can be found in this quote: '%q%'",
-                "This makes the %l% seem more %a% and %a%, as shown in the following quote: '%q%'"
+                "This makes the %l% seem more %a% and %a%, as shown in the following quote: '%q%'",
+                "He does this in the following quote: '%q%', which brings out the %a% nature of the situation in the %l%."
             ]
         ];
         var explanations = [
             [
-                "This suggests that %w% feels very strongly about the situation, as shown by the %t% in the above quote. It causes the reader to associate the two things, which adds to the %a% atmosphere of the %l%.",
+                "This suggests that %w% feels very strongly about the situation, as shown by the %t% in the above quote. It causes the reader to associate the %n% and %n%, which adds to the %a% atmosphere of the %l%.",
                 "'%qw%' suggests that %p% felt extremely %a% about the events in the %l%."
             ],[
                 "Also, the writer effectively uses %t% in his %l%, to let the reader experience %p%'s %a% feelings.",
-                "However, '%qw%' suggests that %p% also felt %a% and %a%, which builds up the reader's interest and makes the %l% more exciting."
+                "However, '%qw%' suggests that %p% also felt %a% and %a%, which builds up the reader's interest and makes the %l% more exciting.",
+                "Furthermore, '%qw%' suggests to the reader the central concept in the %l%, which is %w%'s main intention. He accentuates %p%'s %n%, and puts less emphasis on his %n% to create this effect."
             ]
         ];
         var finalEssay = [];
